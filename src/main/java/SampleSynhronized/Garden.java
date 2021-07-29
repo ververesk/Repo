976ace1dc28ea7784bc3cb3770842f804babb12e
Veronika Hryharovich <weronnika@list.ru>
2021-07-29 14:14:33 +0300

    доделать

---
+++ src/main/java/SampleSynhronized/Garden.java
@@ -0,0 +1,16 @@
+package SampleSynhronized;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Garden {
+    public static void main(String[] args) {
+
+    }
+    public static class CardenNew {
+        public final List<String> fruits=new ArrayList<String>();
+        public final List<String> vegetables=new ArrayList<String>();
+
+
+    }
+}
